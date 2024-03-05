3b8db2f9bb0493112e5b8fed0c993230a63eae0d
Yvonne An <yvonnean8@berkeley.edu>
2024-03-05 02:11:05 -0800

    Initial commit of FakeMonkeytypeUI project

---
+++ swift-playground/FakeMonkeytypeUI/FakeMonkeytypeUI/FakeMonkeytypeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeMonkeytypeUIApp.swift
+//  FakeMonkeytypeUI
+//
+//  Created by Yvonne An on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeMonkeytypeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
